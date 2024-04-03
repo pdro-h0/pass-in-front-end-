1ff4e4541c7d07efbcce90a6e6729870c60cb92a
pdro-h0 <pedro.pho792@gmail.com>
2024-04-03 15:02:17 -0300

    feat: add pagination

--- src/components/AttendeeList.tsx
+++ src/components/AttendeeList.tsx
@@ -11,8 +11,37 @@ import Table from "./table/Table";
 import TableHeader from "./table/TableHeader";
 import TableCells from "./table/TableCells";
 import TableRow from "./table/TableRow";
+import { ChangeEvent, useState } from "react";
+import { attendees } from "../data/attendees";
+import { formatDistanceToNow } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 const AttendeeList = () => {
+  const [search, setSearch] = useState<string>("");
+  const [page, setPage] = useState<number>(1);
+
+  const totalPages = Math.ceil(attendees.length / 10);
+
+  const goToNextPage = () =>{
+    setPage((prevPage) => prevPage + 1 )
+  }
+
+  const goToPrevPage = () =>{
+    setPage((prevPage) => prevPage - 1 )
+  }
+
+  const goToFirstPage = () =>{
+    setPage(1)
+  }
+
+  const goToLastPage = () =>{
+    setPage(totalPages)
+  }
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -20,6 +49,7 @@ const AttendeeList = () => {
         <div className="w-72 px-3 py-1.5 border flex items-center gap-3 border-white/10 bg-transparent rounded-lg text-sm">
           <Search className="size-4 text-emerald-300" />
           <input
+            onChange={handleSearch}
             type="text"
             className="flex-1 bg-transparent outline-none border-0 text-sm p-0"
             placeholder="Buscar participantes..."
@@ -54,36 +84,45 @@ const AttendeeList = () => {
             <TableHeader
               style={{ width: 64 }}
               className="py-3 px-4 text-sm font-semibold text-left"
-            >.</TableHeader>
+            >
+              .
+            </TableHeader>
           </tr>
         </thead>
 
         <tbody>
-          {Array.from({ length: 10 }).map(() => {
+          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
             return (
-              <TableRow className="border-b border-white/10 hover:bg-white/5">
+              <TableRow
+                key={attendee.id}
+                className="border-b border-white/10 hover:bg-white/5"
+              >
                 <TableCells className="py-3 px-4 text-sm  text-left text-zinc-300">
                   <input
                     type="checkbox"
                     className="size-4 checked:text-orange-400 bg-black/20 rounded border border-white/10"
                   />
                 </TableCells>
-                <TableCells>
-                  5432
-                </TableCells>
+                <TableCells>{attendee.id}</TableCells>
                 <TableCells>
                   <div className="flex flex-col gap-1">
                     <span className="font-semibold text-white">
-                      Pedro Henrique de Almeida
+                      {attendee.name}
                     </span>
-                    <span>pedro@test.com.br</span>
+                    <span>{attendee.email.toLocaleLowerCase()}</span>
                   </div>
                 </TableCells>
                 <TableCells>
-                  7 dias atras
+                  {formatDistanceToNow(attendee.createdAt, {
+                    locale: ptBR,
+                    addSuffix: true,
+                  })}
                 </TableCells>
                 <TableCells>
-                  3 dias atras
+                  {formatDistanceToNow(attendee.checkedInAt, {
+                    locale: ptBR,
+                    addSuffix: true,
+                  })}
                 </TableCells>
                 <TableCells>
                   <IconButton transparent={true}>
@@ -98,26 +137,28 @@ const AttendeeList = () => {
         <tfoot>
           <tr>
             <TableCells colSpan={3}>
-              Mostrando 10 de 229 items
+              Mostrando 10 de {attendees.length} items
             </TableCells>
             <TableCells
               className="py-3 px-4 text-sm  text-right text-zinc-300"
               colSpan={3}
             >
               <div className="inline-flex items-center gap-8">
-                <span>Página 1 de 23</span>
+                <span>
+                  Página {page} de {totalPages}
+                </span>
 
                 <div className="flex gap-1.5">
-                  <IconButton>
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
                     <ChevronsLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToPrevPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToNextPage} disabled={page === totalPages}>
                     <ChevronRight className="size-4" />
                   </IconButton>
-                  <IconButton>
+                  <IconButton onClick={goToLastPage} disabled={page === totalPages}>
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
--- src/components/IconButton.tsx
+++ src/components/IconButton.tsx
@@ -2,18 +2,21 @@ import { ComponentProps, ReactNode } from "react";
 
 interface IconButtonProps extends ComponentProps<"button"> {
   children: ReactNode;
-  transparent?: boolean
+  transparent?: boolean;
 }
 
 const IconButton = ({ children, transparent, ...props }: IconButtonProps) => {
+  const { disabled } = { ...props };
+
   return (
     <button
       {...props}
-      className={
+      className={`   ${
         transparent
-        ? "bg-black border border-white/10 rounded-lg p-1.5"
-        : " bg-white/10 border border-white/10 rounded-lg p-1.5"
+          ? "bg-black border border-white/10 rounded-lg p-1.5"
+          : " bg-white/10 border border-white/10 rounded-lg p-1.5"
       }
+        ${disabled ? "cursor-not-allowed opacity-50" : "cursor-pointer"}`}
     >
       {children}
     </button>
